8796d1428a9c62029b418279f4ca19e0a481c5d1
Gemmey <31800978+MahmoudG-Kotp@users.noreply.github.com>
2019-10-27 05:21:49 +0200

    Upload Speaker Driver

---
+++ Speaker.py
@@ -0,0 +1,40 @@
+"""
+Library Link : https://pypi.org/project/pyttsx3/
+***RASPBERRY PI PACKAGES***
+   Espeak >> sudo apt-get install espeak
+
+***PYTHON INSTALLED PACKAGES***
+   Pyttsx3 >> pip3 install pyttsx3
+"""
+import self
+import pyttsx3
+
+class Speaker :
+    __speakerEngine = pyttsx3.init()
+    __RATE_KEY = 'rate'
+    __VOLUME_KEY = 'volume'
+    __VOICE_KEY = 'voices'
+
+    def setSpeechRate(self, rate):
+        self.__speakerEngine.setProperty(self.__RATE_KEY, rate)
+
+    def getSpeechRate(self):
+        return self.__speakerEngine.getProperty(self.__RATE_KEY)
+
+    def setVolume(self, volume):
+        self.__speakerEngine.setProperty(self.__VOLUME_KEY, volume)
+
+    def getVolume(self):
+        return self.__speakerEngine.getProperty(self.__VOLUME_KEY)
+
+    def setSpeechVoice(self, voiceNum):
+        voiceArray = self.getSpeechVoice()
+        self.__speakerEngine.setProperty(self.__VOICE_KEY, voiceArray[voiceNum].id)
+
+    def getSpeechVoice(self):
+        return self.__speakerEngine.getProperty(self.__VOICE_KEY)
+
+    def say(self, text):
+        self.__speakerEngine.say(text)
+        self.__speakerEngine.runAndWait()
+        self.__speakerEngine.stop()
